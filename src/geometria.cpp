dcc1c0391d04e3a275e79fc10374520bef963135
mckimssongg <soymckimsson286@gmail.com>
2023-09-14 23:39:42 -0600

    add

--- src/geometria.cpp
+++ src/geometria.cpp
@@ -1,9 +1,12 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <cmath>
+#include <vector>
+#include <memory>
 
 class Meteor {
 public:
-    Meteor(float x, float y, float z): x(x), y(y), z(z) {}
+    Meteor(float x, float y, float z, float size) : x(x), y(y), z(z), size(size) {}
 
     void move(float dx, float dy, float dz) {
         x += dx;
@@ -14,14 +17,15 @@ public:
     float getX() const { return x; }
     float getY() const { return y; }
     float getZ() const { return z; }
+    float getSize() const { return size; }
 
 private:
-    float x, y, z;
+    float x, y, z, size;
 };
 
 class Spaceship {
 public:
-    Spaceship(float x, float y, float z): x(x), y(y), z(z) {}
+    Spaceship(float x, float y, float z) : x(x), y(y), z(z) {}
 
     void move(float dx, float dy, float dz) {
         x += dx;
@@ -33,10 +37,10 @@ public:
     float getY() const { return y; }
     float getZ() const { return z; }
 
-    bool collidesWithMeteor(const Meteor& meteor, float collisionDistance) const {
+    bool collidesWithMeteor(const Meteor &meteor, float collisionDistance) const {
         float dx = std::abs(x - meteor.getX());
         float dy = std::abs(y - meteor.getY());
-        return (dx < collisionDistance) && (dy < collisionDistance);
+        return (dx < collisionDistance + meteor.getSize() / 2) && (dy < collisionDistance + meteor.getSize() / 2);
     }
 
 private:
@@ -44,6 +48,53 @@ private:
 };
 
 namespace py = pybind11;
+
+class Game {
+public:
+    Game() {}
+
+    void addMeteor(float x, float y, float z, float size) {
+        meteors.push_back(std::unique_ptr<Meteor>(new Meteor(x, y, z, size)));
+    }
+
+    void moveMeteors(float dx, float dy, float dz) {
+        for (auto &meteor : meteors) {
+            meteor->move(dx, dy, dz);
+        }
+    }
+
+    void destroyMeteor(std::size_t index) {
+        if (index < meteors.size()) {
+            meteors.erase(meteors.begin() + index);
+        }
+    }
+
+    bool spaceshipCollidesWithMeteor(const Spaceship &spaceship, float collisionDistance) {
+        for (int i = meteors.size() - 1; i >= 0; i--) {
+            if (spaceship.collidesWithMeteor(*meteors[i], collisionDistance)) {
+                destroyMeteor(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+
+    int getMeteorCount() const {
+        return static_cast<int>(meteors.size());
+    }
+
+    const Meteor& getMeteor(std::size_t index) const {
+        if (index < meteors.size()) {
+            return *meteors[index];
+        } else {
+            throw std::out_of_range("Invalid index");
+        }
+    }
+
+private:
+    std::vector<std::unique_ptr<Meteor>> meteors;
+};
 
 PYBIND11_MODULE(geometria, m) {
     py::class_<Spaceship>(m, "Spaceship")
@@ -55,9 +106,19 @@ PYBIND11_MODULE(geometria, m) {
         .def("collidesWithMeteor", &Spaceship::collidesWithMeteor);
 
     py::class_<Meteor>(m, "Meteor")
-        .def(py::init<float, float, float>())
+        .def(py::init<float, float, float, float>())
         .def("move", &Meteor::move)
         .def("getX", &Meteor::getX)
         .def("getY", &Meteor::getY)
-        .def("getZ", &Meteor::getZ);
+        .def("getZ", &Meteor::getZ)
+        .def("getSize", &Meteor::getSize);
+
+    py::class_<Game>(m, "Game")
+        .def(py::init<>())
+        .def("addMeteor", &Game::addMeteor)
+        .def("moveMeteors", &Game::moveMeteors)
+        .def("destroyMeteor", &Game::destroyMeteor)
+        .def("spaceshipCollidesWithMeteor", &Game::spaceshipCollidesWithMeteor)
+        .def("getMeteorCount", &Game::getMeteorCount)
+        .def("getMeteor", &Game::getMeteor);
 }
--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import random
 import geometria
 import math
 
+projectiles = []
+game = geometria.Game()
+
 def move_spaceship(event):
     x, y = event.x, event.y
     dx, dy = x - spaceship.getX(), y - spaceship.getY()
@@ -10,38 +13,85 @@ def move_spaceship(event):
     canvas.coords(spaceship_shape, x-10, y+10, x+10, y+10, x, y-10)
 
 def update_meteors():
-    global meteors, meteor_shapes
-    new_meteors = []
-    new_meteor_shapes = []
-    
-    if random.random() < 0.5:
-        new_meteor = geometria.Meteor(random.randint(0, 400), 0, 0)
-        x, y = new_meteor.getX(), new_meteor.getY()
-        size = 5
-        points = [(round(x + size * math.cos(math.radians(angle))), round(y + size * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
-        flat_points = [coord for point in points for coord in point]
-        new_shape = canvas.create_polygon(*flat_points, fill="red")
-        meteors.append(new_meteor)
-        meteor_shapes.append(new_shape)
-    
-    for meteor, shape in zip(meteors, meteor_shapes):
-        meteor.move(0, 10, 0)
-        x, y = meteor.getX(), meteor.getY()
-        size = 5
-        points = [(round(x + size * math.cos(math.radians(angle))), round(y + size * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
-        flat_points = [coord for point in points for coord in point]
-        canvas.coords(shape, *flat_points)
-        
-        collision_distance = 15.0
-        if spaceship.collidesWithMeteor(meteor, collision_distance):
-            canvas.delete(shape)
-        else:
-            new_meteors.append(meteor)
-            new_meteor_shapes.append(shape)
+    global meteor_shapes
 
-    meteors = new_meteors
-    meteor_shapes = new_meteor_shapes
+    game.moveMeteors(0, 10, 0)
+
+    if random.random() < 0.5:
+        x = random.randint(0, 400)
+        y = 0
+        z = 0
+        size = 10 
+        game.addMeteor(x, y, z, size)
+        points = [(round(x + size * math.cos(math.radians(angle))), round(y + size * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
+        flat_points = [coord for point in points for coord in point]
+        new_shape = canvas.create_polygon(*flat_points, outline="red", fill="red")
+        meteor_shapes.append(new_shape)
+
+    existing_meteors = []
+
+    for i in range(len(meteor_shapes)):
+        meteor_shape = meteor_shapes[i]
+
+        if canvas.coords(meteor_shape):
+            meteor = game.getMeteor(i)
+            meteor.move(0, 10, 0)
+            x, y = meteor.getX(), meteor.getY()
+            size = 10
+            points = [(round(x + size * math.cos(math.radians(angle))), round(y + size * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
+            flat_points = [coord for point in points for coord in point]
+            canvas.coords(meteor_shape, *flat_points)
+
+            collision_distance = 15.0
+            if spaceship.collidesWithMeteor(meteor, collision_distance):
+                canvas.delete(meteor_shape)
+                game.destroyMeteor(i)
+            else:
+                existing_meteors.append(meteor)
+
+    meteor_shapes = [meteor_shapes[i] for i in range(len(meteor_shapes)) if canvas.coords(meteor_shapes[i])]
+    meteor_shapes += [new_shape for new_shape in meteor_shapes if new_shape not in meteor_shapes]
+
     root.after(500, update_meteors)
+
+def fire_projectile(event):
+    x, y = spaceship.getX(), spaceship.getY()
+    size = 5
+    projectile = canvas.create_oval(x - size, y - size, x + size, y + size, fill="yellow")
+    projectiles.append(projectile)
+
+def update_projectiles():
+    global projectiles
+
+    new_projectiles = []
+    for projectile in projectiles:
+        canvas.move(projectile, 0, -10)
+        x, y, *_ = canvas.coords(projectile)
+
+        meteor_hit = False
+        meteor_to_remove = None
+
+        for i in range(len(meteor_shapes)):
+            meteor_shape = meteor_shapes[i]
+
+            if canvas.coords(meteor_shape):
+                coords = canvas.coords(meteor_shape)
+                x1, y1, x2, y2, *_ = coords
+
+                if x1 <= x <= x2 and y1 <= y <= y2:
+                    meteor_hit = True
+                    meteor_to_remove = i
+                    break
+
+        if meteor_hit:
+            canvas.delete(meteor_shapes[meteor_to_remove])
+            game.destroyMeteor(meteor_to_remove)
+            canvas.delete(projectile)
+        else:
+            new_projectiles.append(projectile)
+
+    projectiles = new_projectiles
+    root.after(100, update_projectiles)
 
 spaceship = geometria.Spaceship(200.0, 200.0, 0.0)
 root = tk.Tk()
@@ -50,16 +100,10 @@ canvas = tk.Canvas(root, width=400, height=400, bg="black")
 canvas.pack()
 x, y = spaceship.getX(), spaceship.getY()
 spaceship_shape = canvas.create_polygon(x-10, y+10, x+10, y+10, x, y-10, fill="yellow")
-meteors = [geometria.Meteor(random.randint(0, 400), random.randint(0, 200), 0) for _ in range(10)]
 meteor_shapes = []
-for meteor in meteors:
-    x, y = meteor.getX(), meteor.getY()
-    size = 5
-    points = [(round(x + size * math.cos(math.radians(angle))), round(y + size * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
-    flat_points = [coord for point in points for coord in point]
-    shape = canvas.create_polygon(*flat_points, fill="red")
-    meteor_shapes.append(shape)
 
 canvas.bind("<Motion>", move_spaceship)
+root.bind("<space>", fire_projectile)
 root.after(500, update_meteors)
+root.after(100, update_projectiles)
 root.mainloop()
