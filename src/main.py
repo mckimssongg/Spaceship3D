badadac3e3cd1be69f5b8324222c54a866a2088e
mckimssongg <soymckimsson286@gmail.com>
2023-09-14 23:52:05 -0600

    resolve: meteor_hit

--- src/main.py
+++ src/main.py
@@ -14,43 +14,32 @@ def move_spaceship(event):
 
 def update_meteors():
     global meteor_shapes
-
     game.moveMeteors(0, 10, 0)
 
     if random.random() < 0.5:
         x = random.randint(0, 400)
         y = 0
         z = 0
-        size = 10 
+        size = 10
         game.addMeteor(x, y, z, size)
         points = [(round(x + size * math.cos(math.radians(angle))), round(y + size * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
         flat_points = [coord for point in points for coord in point]
         new_shape = canvas.create_polygon(*flat_points, outline="red", fill="red")
         meteor_shapes.append(new_shape)
 
-    existing_meteors = []
-
-    for i in range(len(meteor_shapes)):
+    for i in reversed(range(game.getMeteorCount())):
         meteor_shape = meteor_shapes[i]
+        meteor = game.getMeteor(i)
+        x, y = meteor.getX(), meteor.getY()
+        points = [(round(x + 10 * math.cos(math.radians(angle))), round(y + 10 * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
+        flat_points = [coord for point in points for coord in point]
+        canvas.coords(meteor_shape, *flat_points)
 
-        if canvas.coords(meteor_shape):
-            meteor = game.getMeteor(i)
-            meteor.move(0, 10, 0)
-            x, y = meteor.getX(), meteor.getY()
-            size = 10
-            points = [(round(x + size * math.cos(math.radians(angle))), round(y + size * math.sin(math.radians(angle)))) for angle in range(0, 360, 60)]
-            flat_points = [coord for point in points for coord in point]
-            canvas.coords(meteor_shape, *flat_points)
-
-            collision_distance = 15.0
-            if spaceship.collidesWithMeteor(meteor, collision_distance):
-                canvas.delete(meteor_shape)
-                game.destroyMeteor(i)
-            else:
-                existing_meteors.append(meteor)
-
-    meteor_shapes = [meteor_shapes[i] for i in range(len(meteor_shapes)) if canvas.coords(meteor_shapes[i])]
-    meteor_shapes += [new_shape for new_shape in meteor_shapes if new_shape not in meteor_shapes]
+        collision_distance = 15.0
+        if spaceship.collidesWithMeteor(meteor, collision_distance):
+            canvas.delete(meteor_shape)
+            game.destroyMeteor(i)
+            del meteor_shapes[i]
 
     root.after(500, update_meteors)
 
@@ -66,26 +55,27 @@ def update_projectiles():
     new_projectiles = []
     for projectile in projectiles:
         canvas.move(projectile, 0, -10)
-        x, y, *_ = canvas.coords(projectile)
-
+        x, y, x1, y1 = canvas.coords(projectile)
+        x, y = (x + x1) / 2, (y + y1) / 2 
         meteor_hit = False
         meteor_to_remove = None
 
-        for i in range(len(meteor_shapes)):
+        for i in range(game.getMeteorCount()):
             meteor_shape = meteor_shapes[i]
+            coords = canvas.coords(meteor_shape)
+            x_meteor = sum(coords[::2]) / 6 
+            y_meteor = sum(coords[1::2]) / 6
 
-            if canvas.coords(meteor_shape):
-                coords = canvas.coords(meteor_shape)
-                x1, y1, x2, y2, *_ = coords
-
-                if x1 <= x <= x2 and y1 <= y <= y2:
-                    meteor_hit = True
-                    meteor_to_remove = i
-                    break
+            distance = ((x - x_meteor)**2 + (y - y_meteor)**2)**0.5
+            if distance < 15: 
+                meteor_hit = True
+                meteor_to_remove = i
+                break
 
         if meteor_hit:
             canvas.delete(meteor_shapes[meteor_to_remove])
             game.destroyMeteor(meteor_to_remove)
+            del meteor_shapes[meteor_to_remove]
             canvas.delete(projectile)
         else:
             new_projectiles.append(projectile)
